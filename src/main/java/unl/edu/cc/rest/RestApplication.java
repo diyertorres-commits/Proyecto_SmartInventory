62929db8856155b0cdb6712615a3f8fb0e3f8654
YimmyOnner <yimmyonner@gmail.com>
2026-07-15 12:58:33 -0500

    dded

---
+++ src/main/java/unl/edu/cc/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package unl.edu.cc.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
