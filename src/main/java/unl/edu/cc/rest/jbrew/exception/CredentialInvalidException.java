5f2ff8a709d2421239ea0457f296e5597c9548db
Diyer Torres <diyer.torres@unl.edu.ec>
2026-07-27 17:47:22 -0500

    Mejorando del login

--- src/main/java/unl/edu/cc/rest/jbrew/exception/CredentialInvalidException.java
+++ src/main/java/unl/edu/cc/rest/jbrew/exception/CredentialInvalidException.java
@@ -1,11 +1,14 @@
 package unl.edu.cc.rest.jbrew.exception;
 
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException(rollback = true)
 public class CredentialInvalidException extends RuntimeException {
-    
+
     public CredentialInvalidException(String message) {
         super(message);
     }
-    
+
     public CredentialInvalidException(String message, Throwable cause) {
         super(message, cause);
     }
