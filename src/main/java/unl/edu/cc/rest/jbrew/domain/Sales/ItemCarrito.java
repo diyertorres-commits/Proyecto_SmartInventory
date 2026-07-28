c504272028ba23f53331d812b7e1d7fd281e2017
Diyer Torres <diyer.torres@unl.edu.ec>
2026-07-28 00:49:04 -0500

    Arreglando la seccion de compras

--- src/main/java/unl/edu/cc/rest/jbrew/domain/Sales/ItemCarrito.java
+++ src/main/java/unl/edu/cc/rest/jbrew/domain/Sales/ItemCarrito.java
@@ -1,44 +1,113 @@
 package unl.edu.cc.rest.jbrew.domain.Sales;
 
+
+
 import unl.edu.cc.rest.jbrew.domain.Inventory.Product;
+
+
 
 import java.io.Serializable;
 
+
+
 public class ItemCarrito implements Serializable {
 
+
+
     private final int productoId;
+
     private final String productoNombre;
+
     private final double precio;
+
     private int cantidad;
 
+
+
     public ItemCarrito(Product producto, int cantidad) {
+
         this.productoId = producto.getIdProduct();
+
         this.productoNombre = producto.getName();
+
         this.precio = producto.getSalePrice();
+
         this.cantidad = cantidad;
+
     }
+
+
 
     public void incrementarCantidad(int cantidadAdicional) {
+
         this.cantidad += cantidadAdicional;
+
     }
+
+
 
     public int getProductoId() {
+
         return productoId;
+
     }
+
+
 
     public String getProductoNombre() {
+
         return productoNombre;
+
     }
+
+
 
     public double getPrecio() {
+
         return precio;
+
     }
+
+
 
     public int getCantidad() {
+
         return cantidad;
+
     }
 
+
+
     public double getSubtotal() {
+
         return precio * cantidad;
+
     }
+
+
+
+    @Override
+
+    public boolean equals(Object obj) {
+
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        ItemCarrito that = (ItemCarrito) obj;
+
+        return productoId == that.productoId;
+
+    }
+
+
+
+    @Override
+
+    public int hashCode() {
+
+        return Integer.hashCode(productoId);
+
+    }
+
 }
